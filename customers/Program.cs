ef0301fa7dcdb16a42ec7a6ebcd030387c83531a
Martin Baklid <martin.baklid@kredinor.no>
2022-04-01 09:11:13 +0200

    feat: use AddMarten

--- customers/Program.cs
+++ customers/Program.cs
@@ -1,4 +1,5 @@
 using Marten;
+using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IDocumentStore>(
-    DocumentStore.For("host=localhost;database=postgres;password=postgres;username=postgres")
-);
 
+builder.Services.AddMarten(options =>
+    {
+        // Establish the connection string to your Marten database
+        options.Connection(builder.Configuration.GetConnectionString("Marten"));
+
+        // If we're running in development mode, let Marten just take care
+        // of all necessary schema building and patching behind the scenes
+        if (builder.Environment.IsDevelopment())
+        {
+            options.AutoCreateSchemaObjects = AutoCreate.All;
+        }
+    });
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
